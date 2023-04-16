717ba2e39f2bac7e492f147b2fe1b3c7c6814bc5
maangie <maangie906@gmail.com>
2023-04-16 17:35:31 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,64 @@
 import ChatManager from "./chat_manager";
 import getResponse from "./get_response";
-import speak from './speak';
-import { setCookie, getCookie } from './cookie';
+import speak from "./speak";
+import { setCookie, getCookie } from "./cookie";
 
 (() => {
   const $ = (name: string): HTMLElement => <HTMLElement>document.getElementById(name);
 
-  const root: HTMLElement = document.getElementById("root")!;
+  const setUpFocus = (apiKey: HTMLInputElement) => {
+    if (apiKey.value.length) $("user-input").focus();
+    else apiKey.focus();
+  };
 
-  const chatManager = new ChatManager();
-
-  const apiKey = <HTMLInputElement>$('api-key');
-  const userInput = <HTMLInputElement>$('user-input');
-  const submitButton = <HTMLButtonElement>$("submit");
-
-  apiKey.value = getCookie('apiKey');
-
-  if (apiKey.value.length) $('user-input').focus();
-  else apiKey.focus();
-
-  for (const input of document.querySelectorAll<HTMLInputElement>("input[type='text']")) {
+  const handleEnterKey = (input: HTMLInputElement, submitButton: HTMLButtonElement) => {
     input.onkeydown = (event: KeyboardEvent) => {
       if (event.keyCode === 13) {
         event.preventDefault();
         submitButton.click();
       }
     };
-  }
+  };
 
-  $('submit').onclick = async () => {
+  const handleSubmit = async (
+    chatManager: ChatManager,
+    apiKey: HTMLInputElement,
+    userInput: HTMLInputElement,
+    submitButton: HTMLButtonElement
+  ) => {
     const apiKeyValue = apiKey.value;
-    setCookie('apiKey', apiKeyValue);
+    setCookie("apiKey", apiKeyValue);
 
     submitButton.disabled = true;
-    submitButton.innerText = '処理中...';
+    submitButton.innerText = "処理中...";
 
     if (chatManager.hasMessages()) {
-      chatManager.appendUserMessage(userInput.value)
+      chatManager.appendUserMessage(userInput.value);
       chatManager.appendAssistantAnswer(await getResponse(chatManager.getMessages(), apiKeyValue));
     } else {
       await chatManager.initializeMessages(apiKeyValue);
     }
 
     await speak(chatManager.getLastMessage());
-    $('user-input-container').hidden = false;
-    userInput.value = '';
+    $("user-input-container").hidden = false;
+    userInput.value = "";
     userInput.focus();
 
     submitButton.disabled = false;
-    submitButton.innerText = 'Submit';
-  }
+    submitButton.innerText = "Submit";
+  };
+
+  const apiKey = <HTMLInputElement>$("api-key");
+  const userInput = <HTMLInputElement>$("user-input");
+  const submitButton = <HTMLButtonElement>$("submit");
+
+  const chatManager = new ChatManager();
+
+  apiKey.value = getCookie("apiKey");
+  setUpFocus(apiKey);
+
+  for (const input of document.querySelectorAll<HTMLInputElement>("input[type='text']"))
+    handleEnterKey(input, submitButton);
+
+  $("submit").onclick = () => handleSubmit(chatManager, apiKey, userInput, submitButton);
 })();
